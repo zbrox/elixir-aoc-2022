70b686f39bd0ddf466cdf593ce30c55df3239050
Rostislav Raykov <me@zbrox.com>
2022-12-02 22:09:50 +0100

    refactor a bit in preparation for day 2 part 2

--- lib/2022/day_02.ex
+++ lib/2022/day_02.ex
@@ -49,6 +49,29 @@ defmodule Aoc.Y2022.Day02 do
 
   *What would your total score be if everything goes exactly according to your
   strategy guide?*
+
+  ## --- Part Two ---
+
+  The Elf finishes helping with the tent and sneaks back over to you. "Anyway, the
+  second column says how the round needs to end: `X` means you need to lose, `Y`
+  means you need to end the round in a draw, and `Z` means you need to win. Good
+  luck!"
+
+  The total score is still calculated in the same way, but now you need to figure
+  out what shape to choose so the round ends as indicated. The example above now
+  goes like this:
+
+  - In the first round, your opponent will choose Rock (`A`), and you need the round
+  to end in a draw (`Y`), so you also choose Rock. This gives you a score of 1 + 3 = *4*.
+  - In the second round, your opponent will choose Paper (`B`), and you choose Rock
+  so you lose (`X`) with a score of 1 + 0 = *1*.
+  - In the third round, you will defeat your opponent's Scissors with Rock for a
+  score of 1 + 6 = *7*.
+  Now that you're correctly decrypting the ultra top secret strategy guide, you
+  would get a total score of `*12*`.
+
+  Following the Elf's instructions for the second column, *what would your total
+  score be if everything goes exactly according to your strategy guide?*
   """
   @strategy_code %{"A" => "A", "B" => "B", "C" => "C", "X" => "A", "Y" => "B", "Z" => "C"}
 
@@ -58,14 +81,15 @@ defmodule Aoc.Y2022.Day02 do
     |> String.split("\n")
     |> Enum.map(fn v ->
       String.split(v, " ")
-      |> Enum.map(fn v -> @strategy_code[v] end)
       |> List.to_tuple()
     end)
   end
 
   def calculate_game_score(game) do
+    elf_shape = elem(game, 0)
+    my_shape = @strategy_code[elem(game, 1)]
     outcome =
-      case game do
+      case {elf_shape, my_shape} do
         {elf, me} when elf == me -> 3
         {"A", "C"} -> 0
         {"C", "A"} -> 6
@@ -73,7 +97,7 @@ defmodule Aoc.Y2022.Day02 do
         {elf, me} when elf > me -> 0
       end
 
-    outcome + shape_score(elem(game, 1))
+    outcome + shape_score(my_shape)
   end
 
   def shape_score(shape) do
@@ -93,5 +117,15 @@ defmodule Aoc.Y2022.Day02 do
   def solve_part1() do
     Helpers.get_input(2022, 2)
     |> part1
+  end
+
+  def part2(input) do
+    parse_strategy(input)
+
+  end
+
+  def solve_part2() do
+    Helpers.get_input(2022, 2)
+    |> part2
   end
 end
