01dbfcde8b4bfa467b4264bca8b395035158a959
Rostislav Raykov <me@zbrox.com>
2022-12-02 21:53:17 +0100

    add solution for day 2 part 1

---
+++ test/2022/day_02_test.exs
@@ -0,0 +1,16 @@
+defmodule AocTest.Y2022.Day02 do
+  use ExUnit.Case
+  doctest Aoc.Y2022.Day02, import: true
+
+  alias Aoc.Y2022.Day02
+
+  @example """
+  A Y
+  B X
+  C Z
+  """
+
+  test "part1" do
+    assert Day02.part1(@example) == 15
+  end
+end
