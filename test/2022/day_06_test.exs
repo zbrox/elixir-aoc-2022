cf0ec166761994ae51025296211006f895abb01a
Rostislav Raykov <me@zbrox.com>
2022-12-10 18:21:50 +0100

    add solution for day 6 part 1

---
+++ test/2022/day_06_test.exs
@@ -0,0 +1,15 @@
+defmodule AocTest.Y2022.Day06 do
+  use ExUnit.Case
+  doctest Aoc.Y2022.Day06, import: true
+
+  alias Aoc.Y2022.Day06
+
+  test "part1" do
+    assert Day06.part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7
+    assert Day06.part1("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5
+    assert Day06.part1("nppdvjthqldpwncqszvftbrmjlhg") == 6
+    assert Day06.part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10
+    assert Day06.part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11
+  end
+
+end
